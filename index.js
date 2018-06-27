2fd4a0c63011380935d99e07dba20825c585e5c0
parisa pahlevan <paris.pahlevan@gmail.com>
2018-06-27 02:16:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,31 +1,47 @@
-
-function takeANumber(katzDeliLine, name){
-  katzDeliLine.push(name)
-  var p = katzDeliLine.length
-  return `Welcome, ${name}. You are number ${p} in line.`
+var katzDeliLine = ["jshfu", "jhg", "jnh"]
+i = 0
+function takeANumber(katzDeliLine, i){
+katzDeliLine.push(i)
+i = i +1
+return `Welcome, You are number ${katzDeliLine.length} on line.`
 }
 
 function nowServing(katzDeliLine){
-  if(katzDeliLine.length == 0){
-    return "There is nobody waiting to be served!"
-  }else{
-    var savedName = katzDeliLine[0]
-    katzDeliLine.shift()
-    return `Currently serving ${savedName}.`
+  if (katzDeliLine.length === 0){
+   return "There is nobody waiting to be served!"
+  } else {
+   var firstPerson = katzDeliLine 
+   katzDeliLine.shift()
   }
+  return `Currently serving ${firstPerson}.`
 }
 
-function currentLine (katzDeliLine) {
-    if(katzDeliLine.length == 0){
-        return "The line is currently empty."
-    }else{
-        var message = "The line is currently: "
-        for(var i=0; i<katzDeliLine.length; i++){
-          message += `${i+1}. ${katzDeliLine[i]}`
-          if(i <katzDeliLine.length-1 ){
-            message += ', '
-          }
-        }
-        return message
-    }
-}
+function currentLine(katzDeliLine){
+ if (katzDeliLine.length === 0) {
+ return "The line is currently empty."  
+ } else {
+  var array = []
+  for (var i = 0; i < katzDeliLine.length; i++){
+  var myClients = `${i + 1}. ${katzDeliLine[i]}`
+  array.push(myClients);
+  }
+  return `The line is currently: ${array.join(', ')}`
+ }
+ }
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+
